ec14339e66b1077bd2b8c1b53c1fa1178dcae77d
Rapha <raphanicaise@gmail.com>
2024-08-11 23:53:33 -0300

    add: Functions in utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import pyarrow as pa
 from deltalake import DeltaTable,write_deltalake
-
+import json
 
 def get_data(url_base,endpoint,params=None,headers=None):
     """
@@ -28,4 +28,30 @@ def get_data(url_base,endpoint,params=None,headers=None):
     
     return data
 
+# Funciones para obtener y actualizar la metadata
+
+def update_last_update_in_json(path,new_value):
+    """
+        Actualizar el valor del json, con la fecha del ultimo partido cargado
+    Args:
+        path (str): path del archivo json
+        new_value (str): Nueva fecha del ultimo partido cargado
+    """
+    with(open(path,'w')) as file:
+        json.dump(new_value,file)
+
+def get_metadata_from_json(path):
+    """
+        Obtiene la metadata del json
+    Args:
+        path (_type_): path del archivo json
+
+    Returns:
+        str: la fecha del ultimo partido cargado
+    """
+    
+    with open(path) as f:
+        metadata = json.load(f)
+
+    return metadata
 
